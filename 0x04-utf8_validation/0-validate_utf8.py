5041b596cbc81f913809d8a9ac22cc700a3edc83
underdogs180 <asadirshad38@gmailcom>
2022-05-05 07:10:17 +0000

    Task

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,34 +3,34 @@
 
 
 def validUTF8(data):
-    # checks if most significant byte is 1
-    mask1 = 1 << 7
-    # checks if second most significant byte is 0
-    mask2 = 1 << 6
-    # keeps track of how many 1s before 0 occurs
-    n_bytes = 0
+# checks if most significant byte is 1
+mask1 = 1 << 7
+# checks if second most significant byte is 0
+mask2 = 1 << 6
+# keeps track of how many 1s before 0 occurs
+n_bytes = 0
 
-    if not data or len(data) == 0:
-        return True
+if not data or len(data) == 0:
+return True
 
     for num in data:
-        mask = 1 << 7
-        if n_bytes == 0:
-            while (mask & num):
-                n_bytes += 1
-                mask = mask >> 1
+    mask = 1 << 7
+    if n_bytes == 0:
+    while (mask & num):
+    n_bytes += 1
+    mask = mask >> 1
 
-            if n_bytes == 0:
-                continue
-            if n_bytes == 1 or n_bytes > 4:
-                return False
-        else:
+    if n_bytes == 0:
+    continue
+    if n_bytes == 1 or n_bytes > 4:
+    return False
+    else:
 
-             if not (num & mask1 and not (num & mask2)):
-                 return False
-        n_bytes -= 1
+    if not (num & mask1 and not (num & mask2)):
+    return False
+    n_bytes -= 1
 
     if n_bytes:
-        return False
+    return False
     else:
-        return True
+    return True

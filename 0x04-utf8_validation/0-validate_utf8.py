b31098c53e201838a5fdbeb1661893a1525e539e
underdogs180 <asadirshad38@gmailcom>
2022-05-05 07:23:17 +0000

    Task

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,36 +1,28 @@
 #!/usr/bin/python3
-""" determines if a given data set represents a valid UTF-8 encoding """
+"""UTF-8 Validator
+"""
 
 
 def validUTF8(data):
-# checks if most significant byte is 1
-mask1 = 1 << 7
-# checks if second most significant byte is 0
-mask2 = 1 << 6
-# keeps track of how many 1s before 0 occurs
-n_bytes = 0
-
-if not data or len(data) == 0:
-return True
-
-    for num in data:
-    mask = 1 << 7
-    if n_bytes == 0:
-    while (mask & num):
-    n_bytes += 1
-    mask = mask >> 1
-
-    if n_bytes == 0:
-    continue
-    if n_bytes == 1 or n_bytes > 4:
+    """utf-8 validator
+    """
+    count = 0
+    for d in data:
+        if count == 0:
+            if d & 128 == 0:
+                count = 0
+            elif d & 224 == 192:
+                count = 1
+            elif d & 240 == 224:
+                count = 2
+            elif d & 248 == 240:
+                count = 3
+            else:
+                return False
+        else:
+            if d & 192 != 128:
+                return False
+            count -= 1
+    if count == 0:
+        return True
     return False
-    else:
-
-    if not (num & mask1 and not (num & mask2)):
-    return False
-    n_bytes -= 1
-
-    if n_bytes:
-    return False
-    else:
-    return True
